5cc08729d80b650a2da66035f9790343ed3edd70
fzanollo <fzanollo@dc.uba.ar>
2023-02-16 21:46:19 -0300

    test failing

--- tests/test_age.py
+++ tests/test_age.py
@@ -11,7 +11,8 @@ class TestAge(TestCase):
         age_var = Age()  # TODO add context examples for age
 
         expected = pd.read_csv("tests/resources/age_posterior_v1.csv").set_index(['FC', 'MP'])['likelihood']
-        obtained = age_var.get_posterior("Standard")
+        min_age, max_age, category_ranges = age_var._get_config_from_sigmas()
+        obtained = age_var.get_likelihood_v1(min_age, max_age, category_ranges)
 
         self.assertEqual(expected.shape, obtained.shape, "different shape")
 
